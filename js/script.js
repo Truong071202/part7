e485c1cb150fe71e4bf8bb98159ff2fcdc1a6354
Truong <truongdang2k2dn@gmail.com>
2022-07-26 13:21:34 +0700

    part7

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// const showAlert = () => {
+//     alert('Xin chào');
+// };
+// showAlert();
+// function handleClick(parameter) {
+//     alert('Xin chào'+parameter);
+// }
+// handleClick('hello bitching');
+// document.getElementById('title').onclick =() =>{
+//     handleClick(document.getElementById('title').innerText);
+// };
+// const nameUser = prompt('Nhập tên:');
+// console.log('nameUser',nameUser);
+// const isYes = confirm('bạn có đi học không');
+// console.log('isYes',isYes);
+// const point = prompt('Mời nhập số điểm', 0);
+// console.log('point',typeof point, point);
+
+const taskList = [];
+// let listValue = '';
+document.getElementById('title').onclick =() =>{
+    const valueInput = document.getElementById('')
+};
